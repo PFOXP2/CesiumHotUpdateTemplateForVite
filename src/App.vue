0d0c06c2c811331893037246f93608d556e368d9
weipeng <pfoxp2@gmail.com>
2025-01-17 15:49:55 +0800

    normal modify

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,7 @@ import * as Cesium from '@/cesiumSource'
 import './cesiumSource/packages/widgets/Source/widgets.css'
 (window as any).CESIUM_BASE_URL = '/cesiumAssets'
 
-function adjustInfoBox() {
-  var iframe = document.getElementsByClassName('cesium-infoBox-iframe')[0]
-  iframe.setAttribute('sandbox', 'allow-same-origin allow-scripts allow-popups allow-forms')
-  iframe.setAttribute('src', '')
-}
-
 onMounted(() => {
-  // adjustInfoBox()
   const viewer = new Cesium.Viewer('cesiumContainer')
 })
 </script>
@@ -27,6 +20,5 @@ onMounted(() => {
   height: 100%;
   margin: 0;
   padding: 0;
-  border: 0px!important;
 }
 </style>
